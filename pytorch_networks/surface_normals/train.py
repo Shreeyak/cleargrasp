798b016c6a9c19c3d9f3a2b8d4527349ad374b40
GainiK <gaini.kussainova@nu.edu.kz>
2020-11-10 18:00:00 +0600

    add aspect ratio change

--- pytorch_networks/surface_normals/train.py
+++ pytorch_networks/surface_normals/train.py
@@ -83,6 +83,7 @@ for line in config_str:
 writer.add_text('Config', string, global_step=None)
 
 ###################### DataLoader #############################
+
 # Train Dataset - Create a dataset object for each dataset in our list, Concatenate datasets, select subset for training
 augs_train = iaa.Sequential([
     # Geometric Augs
